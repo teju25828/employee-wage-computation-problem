b424114ef6bd7cff1fe39d6b774a32d11f57f0fd
Raaj <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>
2022-10-04 01:39:50 +0530

    UC2 commit

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,13 +2,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM...");
+        int wagePerHour=20,fullDayHour=8, wagePerDay;
         int rand = (int) (Math.random() * 2);
         System.out.println(rand);
         if (rand == 1) {
             System.out.println("employee is present");
+            wagePerDay=wagePerHour*fullDayHour;
+            System.out.println("wage for day=> "+wagePerDay);
         } else {
             System.out.println("employee is absent");
         }
+
 
 
     }
