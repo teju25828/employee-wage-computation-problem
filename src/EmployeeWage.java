0c9d046c62d0f3410e9db04367ef77d86ac1eae0
Raaj <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>
2022-10-04 02:12:38 +0530

    UC5 Commit

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,9 +2,9 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM...");
-        int wagePerHour=20,fullDayHour=8, wagePerDay,partTimeHour=4;
-        int rand = (int) (Math.random() * 3);
-        System.out.println(rand);
+        int wagePerHour = 20, fullDayHour = 8, wagePerDay, partTimeHour = 4, workingDaysPerMonth = 20;
+        int rand, wagesForMonth = 0;
+
         /*if (rand == 1) {
             System.out.println("employee is present");
             wagePerDay=wagePerHour*fullDayHour;
@@ -17,20 +17,27 @@ public class EmployeeWage {
         else {
             System.out.println("employee is absent");
         }*/
-        switch(rand)
-        {
-            case 0:
-                System.out.println("employee is Absent");
-                break;
-            case 1:
-                System.out.println("employee is present");
-                wagePerDay=wagePerHour*fullDayHour;
-                System.out.println("wage for day=> "+wagePerDay);
-                break;
-            case 3 :
-                System.out.println("Employee working as part time ");
-                wagePerDay=wagePerHour*partTimeHour;
-                System.out.println("wage for day=> "+wagePerDay);
+        int i = 0;
+        while (i < workingDaysPerMonth) {
+            rand = (int) (Math.random() * 3);
+            switch (rand) {
+                case 0:
+                    System.out.println("Day " + (i + 1) + " employee is Absent");
+                    break;
+                case 1:
+                    System.out.println("Day " + (i + 1) + " employee is present");
+                    wagePerDay = wagePerHour * fullDayHour;
+                    wagesForMonth = wagesForMonth + wagePerDay;
+                    System.out.println("wage for day=> " + wagePerDay);
+                    break;
+                case 3:
+                    System.out.println("Day " + (i + 1) + " Employee working as part time ");
+                    wagePerDay = wagePerHour * partTimeHour;
+                    wagesForMonth = wagesForMonth + wagePerDay;
+                    System.out.println("wage for day=> " + wagePerDay);
+                    break;
+            }
         }
+        System.out.println("The total wage for the month is : "+wagesForMonth);
     }
 }
