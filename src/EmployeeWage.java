174ed985765d063f20ff4d8a503c16891b8cc428
Raaj <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>
2022-10-04 01:36:12 +0530

    UC1 commit

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,5 +2,14 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM...");
+        int rand = (int) (Math.random() * 2);
+        System.out.println(rand);
+        if (rand == 1) {
+            System.out.println("employee is present");
+        } else {
+            System.out.println("employee is absent");
+        }
+
+
     }
 }
